13a4985bda195b7a2686a5cc1ce461bc70e93db6
larisa <laurabrovkina@gmail.com>
2025-05-22 13:43:47 +1200

    Exercise 2.6

--- 2-basic-types/2.6-unknown/exercise.ts
+++ 2-basic-types/2.6-unknown/exercise.ts
@@ -1,9 +1,17 @@
 // Exercise: Working with the `unknown` type in TypeScript
 
 // 1. Declare a variable of type `unknown` and assign a value to it.
-
+const myVariable: unknown = "2";
 // 2. Write a function `isString` that takes an argument of type `unknown`
 // and returns true if the argument is a string.
-
+function isString(param: unknown) {
+    return typeof param === "string";
+}
 // 3. Call the isString function above to check if the `unknownValue` is a string,
 // and if so, log its length to the console.
+if(isString(myVariable)) {
+    console.log(myVariable.length);
+}
+else {
+    console.log(myVariable + " is not a string");
+}
